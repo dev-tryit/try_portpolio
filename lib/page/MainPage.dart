313ad8a2273f058f539fcf30513e150eab9fb370
KimDonghyun <imkim189371@gmail.com>
2022-07-01 23:39:07 +0900

    치킨 먹구와서, 글꼴 적용~

--- lib/page/MainPage.dart
+++ lib/page/MainPage.dart
@@ -43,23 +43,22 @@ class MainPage extends StatelessWidget {
                   Row(
                     children: [
                       Expanded(
-                          child: Column(
-                        children: [
-                          Text("안녕하세요"),
-                          Text("프리랜서 개발자"),
-                          Text("김동현입니다"),
-                          Text(
-                              "생산성 있는 개발과 유지보수하기 좋은 코드를 지향합니다.\n2022년 현재, Dart-Flutter 기술 스택을 활용한 프리랜서로 활동하고 있습니다.\n주요 이력으로는, 네오위즈의 모바일플랫폼개발팀에서 Java-SpringBoot을 통한 서버 관리 업무, Java-Android와 C#-Unity를 통한 SDK 개발 및 유지보수 업무를 진행하였습니다."),
-                        ],
-                      )),
+                        flex: 55,
+                        child: Column(
+                          children: [
+                            Text("안녕하세요"),
+                            Text("프리랜서 개발자"),
+                            Text("김동현입니다"),
+                            Text(
+                                "생산성 있는 개발과 유지보수하기 좋은 코드를 지향합니다.\n2022년 현재, Dart-Flutter 기술 스택을 활용한 프리랜서로 활동하고 있습니다.\n주요 이력으로는, 네오위즈의 모바일플랫폼개발팀에서 Java-SpringBoot을 통한 서버 관리 업무, Java-Android와 C#-Unity를 통한 SDK 개발 및 유지보수 업무를 진행하였습니다."),
+                          ],
+                        ),
+                      ),
                       Expanded(
-                        child: SizedBox(
-                          width: 250,
-                          height: 250,
-                          child: Image(
-                            image: MyImage.randomImage(width: 250, height: 250),
-                            fit: BoxFit.fill,
-                          ),
+                        flex: 45,
+                        child: Image(
+                          image: MyImage.randomImage(width: 250, height: 250),
+                          fit: BoxFit.fill,
                         ),
                       )
                     ],
--- lib/util/MyTheme.dart
+++ lib/util/MyTheme.dart
@@ -1,12 +1,7 @@
-import '../util/MyColors.dart';
+
+import 'package:flutter/material.dart';
 
 class MyTheme{
-  static const backgroundColor = MyColors.grey900;
-  static const menuItemColor = MyColors.grey200;
-  static const greyButtonColor = MyColors.grey100;
-  static const highlightColor = MyColors.lightGreen;
-  static const highlightColor2 = MyColors.purple500;
-  static const black = MyColors.realBlack;
-  static const white = MyColors.realWhite;
+  static const backgroundColor = Colors.white;
 
 }
