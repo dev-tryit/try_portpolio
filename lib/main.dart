cfe7b29296601be94ec3a69431bf23c8fa4860d0
KimDonghyun <imkim189371@gmail.com>
2022-07-02 14:54:01 +0900

    파이어베이스 연동 완료

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,16 @@ import 'page/MainPage.dart';
 import 'util/MyEmoji.dart';
 import 'util/MyStoreUtil.dart';
 import 'util/MyTexts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   ErrorUtil.catchError(() async {
     await MyStoreUtil.init();
     // await AuthUtil.me.init();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
     runApp(MyApp());
   });
